bec14a4d8ae87d5d1fbe5f5ae4c7bed6ddc945ef
Sasi S <sasthumne@gmail.com>
2023-03-02 00:19:09 +0000

    Add Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,6 +33,12 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar ----
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # CloudWatch logs -----
 # Configuring Logger to Use CloudWatch
 #LOGGER = logging.getLogger(__name__)
@@ -63,6 +69,24 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
+# Rollbar --------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 # X-RAY -------------
 #XRayMiddleware(app, xray_recorder)
 
@@ -81,6 +105,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# Rollbar ------
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 # CloudWatch logs -----
 #@app.after_request
