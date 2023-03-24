58903dbebe3a0d40830c924bbc43123b035fd18d
Sasi S <sasthumne@gmail.com>
2023-03-24 01:31:55 +0000

    Creating message group feed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,9 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Sasi', 'sasi' ,'sasi@gmail.com','MOCK'),
-  ('sasi thv', 'thv','sasithv@sk.com' ,'MOCK');
+  ('sasi thv', 'thv','sasithv@sk.com' ,'MOCK'),
+  ('Londo Mollari','londo' ,'lmollari@centari.com' ,'MOCK');
+  
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
